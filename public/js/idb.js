c35b81c5a915732c28cc1e08f47c35854ccdf2be
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-12 05:29:36 -0700

    service-worker

---
+++ public/js/idb.js
@@ -0,0 +1,60 @@
+//const { get } = require("mongoose");
+
+let db;
+
+const request = indexedDB.open('budget',1);
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_item', {autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if(navigator.onLine) {
+        uploadItem();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_item'], 'readwrite');
+    const itemObjectStore = transaction.objectStore('new_item');
+    itemObjectStore.add(record);
+};
+
+function uploadItem() {
+    const transaction = db.transaction(['new_item'], 'readwrite');
+    const itemObjectStore = transaction.objectStore('new_item');
+    const getAll = itemObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_item'], 'readwrite');
+                const itemObjectStore = transaction.objectStore('new_item');
+                itemObjectStore.clear();
+                alert('All saved items have been submitted');
+            })
+            .catch(err=>{
+                console.log(err);
+            });
+        }
+    };
+};
+
+window.addEventListener('online', uploadItem);
